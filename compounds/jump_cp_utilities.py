aa61f9770671135052f5619b7a8fec47440270d6
Steven Luke <steve@thelukes.net>
2024-12-29 21:19:35 -0500

    Test code in prep of the JUMPCP and ChemBL chem data work.

---
+++ compounds/jump_cp_utilities.py
@@ -0,0 +1,7 @@
+from broad_babel.query import run_query
+
+None, 'LRRMQNGSYOUANY-OMCISZLKSA-N', 'compound', 'poscon_cp'),
+#  (None, 'DHMTURDWPRKSOA-RUZDIDTESA-N', 'compound', 'poscon_diverse'),
+#  ...
+#  ('JCP2022_913605', 'CDK2', 'orf', 'poscon_orf'),
+#  ('JCP2022_913622', 'CLK1', 'orf', 'poscon_cp')]
